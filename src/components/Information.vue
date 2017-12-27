ed1bf4251ab78d52cf9fc956b6587062460cfc8c
dengchuanjin <1193021198@qq.com>
2017-12-27 15:25:15 +0800

    first_commit

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -75,7 +75,8 @@
         prop="SFreeRate"
         label="提现费率"
         align="center"
-        width="120">
+        width="120"
+      >
       </el-table-column>
       <el-table-column
         prop="STelephone"
@@ -109,60 +110,62 @@
     <el-dialog title="修改商户" :visible.sync="updateCashUserDialog">
       <el-form :model="newUser"  size="small">
         <el-form-item label="商户编号" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.Sid" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="电话" label-width="140px" :required="isOff">
-          <el-input v-model="newUser.STelephone" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="收款人姓名" label-width="140px" :required="isOff">
-          <el-input v-model="newUser.SWorkerName" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="办卡银行" label-width="140px" :required="isOff">
-          <el-input v-model="newUser.SWorkerBank" auto-complete="off"></el-input>
+          <el-input v-model="newUser.Sid" auto-complete="off" :disabled="true"></el-input>
         </el-form-item>
         <el-form-item label="提现费率" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.SFreeRate" auto-complete="off"></el-input>
+          <el-input v-model="newUser.SFreeRate" auto-complete="off" :disabled="true"
+          ></el-input>
         </el-form-item>
-        <el-form-item label="汇卡卡号" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.SHuiCardNo" auto-complete="off"></el-input>
+        <el-form-item label="电话" label-width="120px" :required="isOff">
+          <el-input v-model="newUser.STelephone" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="机构号" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.SOrganNo" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="机构密钥" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.SOrganSecret" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="微信 APPID" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.APPID" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="微信 APPKEY" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.APPKEY" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="支付回调地址" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.NotifyURL" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="退款回调地址" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.RefundURL" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="商户密钥" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.APPSECRET" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="退款证书路径" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.CertPath" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="证书密码" :label-width="formLabelWidth" :required="isOff">
-          <el-input v-model="newUser.CertPassword" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="收款人姓名" label-width="140px" :required="isOff">
+        <el-form-item label="收款人姓名" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SWorkerName" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="办卡银行" label-width="140px" :required="isOff">
+        <el-form-item label="办卡银行" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SWorkerBank" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="银行卡号" label-width="140px" :required="isOff">
+
+        <!--<el-form-item label="汇卡卡号" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.SHuiCardNo" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="机构号" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.SOrganNo" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="机构密钥" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.SOrganSecret" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="微信 APPID" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.APPID" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="微信 APPKEY" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.APPKEY" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="支付回调地址" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.NotifyURL" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="退款回调地址" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.RefundURL" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="商户密钥" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.APPSECRET" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="退款证书路径" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.CertPath" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="证书密码" :label-width="formLabelWidth" :required="isOff">-->
+          <!--<el-input v-model="newUser.CertPassword" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="收款人姓名" label-width="140px" :required="isOff">-->
+          <!--<el-input v-model="newUser.SWorkerName" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="办卡银行" label-width="140px" :required="isOff">-->
+          <!--<el-input v-model="newUser.SWorkerBank" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <el-form-item label="银行卡号" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SWorkerCard" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="是否每日自动提现" label-width="140px" :required="isOff">
+        <el-form-item label="是否每日自动提现" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SAutoOut" placeholder="是否每日自动提现">
             <el-option
               v-for="item in options"
@@ -172,57 +175,57 @@
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="网售服务费的类型" label-width="140px" :required="isOff">
+        <el-form-item label="网售服务费的类型" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SWebServiceType" placeholder="网售服务费的类型">
             <el-option label="固定设置" value="0"></el-option>
             <el-option label="根据票价" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="网售服务费" label-width="140px" :required="isOff">
+        <el-form-item label="网售服务费" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SWebSellFee" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="联系邮箱" label-width="140px" :required="isOff">
+        <el-form-item label="联系邮箱" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SEmail" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="是否启用添加乘客功能" label-width="140px" :required="isOff">
+        <el-form-item label="是否启用添加乘客功能" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SPassengerUse" placeholder="是否启用添加乘客功能">
             <el-option label="不启用" value="0"></el-option>
             <el-option label="启用" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="打折金额" label-width="140px" :required="isOff">
+        <el-form-item label="打折金额" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SSale" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="是否售半价票" label-width="140px" :required="isOff">
+        <el-form-item label="是否售半价票" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SSaleHalf" placeholder="是否售半价票">
             <el-option label="不售" value="0"></el-option>
             <el-option label="售" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="是否售免票" label-width="140px" :required="isOff">
+        <el-form-item label="是否售免票" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SSaleFree" placeholder="是否售免票">
             <el-option label="不售" value="0"></el-option>
             <el-option label="售" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="是否读取全部班次" label-width="140px" :required="isOff">
+        <el-form-item label="是否读取全部班次" label-width="120px" :required="isOff">
           <el-select v-model="newUser.SAllSale" placeholder="是否读取全部班次">
             <el-option label="不全读" value="0"></el-option>
             <el-option label="全读" value="1"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="班次提前多少分钟可微信购票" label-width="210px" :required="isOff">
+        <el-form-item label="班次提前多少分钟可微信购票" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SSellTimeLimit" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="商户简介" label-width="140px" :required="isOff">
+        <el-form-item label="商户简介" label-width="120px" :required="isOff">
           <el-input v-model="newUser.SIntroduce" auto-complete="off"></el-input>
         </el-form-item>
-        <el-form-item label="支付方式" label-width="140px" :required="isOff">
-          <el-input v-model="newUser.SPayType" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="服务商下的子商户" label-width="140px" :required="isOff">
-          <el-input v-model="newUser.SubMchid" auto-complete="off"></el-input>
-        </el-form-item>
+        <!--<el-form-item label="支付方式" label-width="140px" :required="isOff">-->
+          <!--<el-input v-model="newUser.SPayType" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
+        <!--<el-form-item label="服务商下的子商户" label-width="140px" :required="isOff">-->
+          <!--<el-input v-model="newUser.SubMchid" auto-complete="off"></el-input>-->
+        <!--</el-form-item>-->
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="updateCashUserDialog = false">取 消</el-button>
